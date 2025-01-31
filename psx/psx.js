ea4990b96110a530e725fe85c304346606629d0b
Marc Robledo <mark@tinet.org>
2025-01-31 18:29:56 +0100

    added proper license text in JS files

--- psx/psx.js
+++ psx/psx.js
@@ -1,4 +1,33 @@
-/* PSX module for Memory Card Manager JS v20230314 - Marc Robledo 2019-2023 - http://www.marcrobledo.com/license */
+/*
+* Gamecube implementation for Memory Card Manager JS
+*
+* By Marc Robledo https://www.marcrobledo.com
+* Sourcecode: https://github.com/marcrobledo/memory-card-manager-js/
+* License:
+*
+* MIT License
+* 
+* Copyright (c) 2019-2025 Marc Robledo
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
 /*
 	File format specification:
 	https://www.psdevwiki.com/ps3/PS1_Savedata#Memory_card_PS1_.28original.29
